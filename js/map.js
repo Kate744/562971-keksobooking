d5cda53f25cd55c0796e88f552a20aee6fd4cb19
Kate744 <kate744@ya.ru>
2018-02-15 16:37:40 +0300

    Оо

--- js/map.js
+++ js/map.js
@@ -1,10 +1,7 @@
 'use strict';
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
-var CENTER_PIN_WIDTH = 40 / 2;
-var PIN_HEIGTH = 44 - 40;
+var PIN_WIDTH = 40;
+var CENTER_PIN_HEIGHT = 40 / 2;
 var TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира',
   'Огромный прекрасный дворец', 'Маленький ужасный дворец',
   'Красивый гостевой домик', 'Некрасивый негостеприимный домик',
@@ -26,7 +23,11 @@ var AVATARS = ['img/avatars/user01.png',
   'img/avatars/user07.png',
   'img/avatars/user08.png'];
 var COUNT = 8;
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
+
+var map = document.querySelector('.map');
 // пустой массив для обьектов
 var offers = [];
 
@@ -60,8 +61,8 @@ function getFeatures() {
   return array;
 }
 
-// создаем функцию, генерирующую массив из обьекта со случайными данными
-function getAds() {
+// создаем функцию, генерирующую массив из обьектов со случайными данными
+function getDataAds() {
   var mixTitles = shuffle(TITLE);
   var mixAvatars = shuffle(AVATARS);
 
@@ -74,7 +75,7 @@ function getAds() {
       },
       offer: {
         title: mixTitles[i],
-        address: x + ',' + y,
+        address: x + ', ' + y,
         price: getRandomNumberInRange(1000, 1000000),
         type: getRandomElement(TYPE),
         rooms: getRandomElement(ROOMS),
@@ -95,7 +96,7 @@ function getAds() {
   }
 }
 // запускаем функцию для заполнения массива 8 обьектами
-getAds();
+getDataAds();
 
 // создаем фрагмент для создания пинов, обозначающих расположение созданных 8 обьектов
 var fragment = document.createDocumentFragment();
@@ -105,19 +106,23 @@ function makePins() {
     var templateElement = document.querySelector('template').content;
     var btn = templateElement.querySelector('.map__pin').cloneNode(true);
     var image = btn.querySelector('img');
-
+    var data = offers[i];
     // добавляем координаты х, у и картинку в button
-    btn.style.left = (offers[i].location.x - CENTER_PIN_WIDTH) + 'px';
-    btn.style.top = (offers[i].location.y - PIN_HEIGTH) + 'px';
-    image.src = offers[i].author.avatar;
+    btn.style.left = (data.location.x - PIN_WIDTH) + 'px';
+    btn.style.top = (data.location.y - CENTER_PIN_HEIGHT) + 'px';
+    image.src = data.author.avatar;
+
+    btn.addEventListener('click', function () {
+      generateCard(data);
+    });
 
     fragment.appendChild(btn);
   }
 }
-makePins();
 
+makePins();
 var putMapPins = document.querySelector('.map__pins');
-putMapPins.appendChild(fragment);
+// putMapPins.appendChild(fragment);
 
 // Определение типа жилья для русификации слов
 var TYPES = {
@@ -155,7 +160,9 @@ function addPhotos(el, data) {
 }
 // функция, заполняющая карточку выбранного обьекта
 // сейчас на основе первого по порядку элемента из сгенерированного массива
-function generateAd(data) {
+
+
+function generateCard(data) {
   var templateElement = document.querySelector('template').content;
   var putIn = templateElement.querySelector('.popup').cloneNode(true);
   var pElements = putIn.querySelectorAll('p');
@@ -185,6 +192,61 @@ function generateAd(data) {
   map.insertBefore(putIn, mapFilters);
 }
 
-generateAd(offers[0]);
-
 var mapFilters = map.querySelector('.map__filters-container');
+
+
+// Задание по событиям
+
+map.classList.add('map--faded');
+// отключаем активность у полей
+// для начального экрана
+
+var form = document.querySelector('.notice__form');
+form.classList.add('notice__form--disabled');
+
+// ищем стартовый пин
+var startPin = document.querySelector('.map__pin--main');
+
+function mouseupOnStartPin() {
+  map.classList.remove('map--faded');
+  startPin.removeAttribute('disabled');
+  form.classList.remove('notice__form--disabled');
+  putMapPins.appendChild(fragment);
+  // generateCard(offers[3]);
+  var fieldsets = form.querySelectorAll('fieldset');
+  for (var i = 0; i < fieldsets.length; i++) {
+    fieldsets[i].removeAttribute('disabled');
+  }
+}
+startPin.addEventListener('mouseup', function () {
+  mouseupOnStartPin();
+  getAddress();
+});
+
+// ищем поле адреса и заполняем его по умолчанию
+var notice = document.querySelector('.notice__form');
+notice.querySelector('#address');
+function getAddress() {
+
+}
+
+
+// закрыть карточку кликом или клавишей
+var findCardClose = document.querySelector('.popup__close');
+findCardClose.addEventListener('click', closeCard);
+
+function closeCard() {
+  findCardClose.setAttribute('disabled', 'disabled');
+  document.addEventListener('keydown', onCardClose);
+}
+function onCardClose(evt) {
+  if (findCardClose === document.activeElement) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      closeCard();
+    }
+  } else {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closeCard();
+    }
+  }
+}
